88cda961bcd037c7f28ba3274aeaa7a8ee3426ec
Mark Kariuki <kariukimarkg@gmail.com>
2024-07-22 23:58:02 +0300

    updates

--- android/app/src/main/java/com/example/sandbox_receiver/MainActivity.java
+++ android/app/src/main/java/com/example/sandbox_receiver/MainActivity.java
@@ -1,51 +1,34 @@
 
 package com.example.sandbox_receiver;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Bundle;
-import android.util.Log;
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
-    private static final String CHANNEL = "com.example.keyevents/receiveKeyEvent";
-    private static final String TAG = "MainActivity";
-    private static final String ACTION_CUSTOM_BROADCAST = "com.example.sandbox";
-    private MethodChannel methodChannel;
-    private KeyEventReceiver mReceiver;
+    private static final String CHANNEL = "com.example.sandbox_receiver/refresh";
 
     @Override
-    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        super.configureFlutterEngine(flutterEngine);
-
-        methodChannel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL);
-        mReceiver = new KeyEventReceiver(methodChannel);
-
-        // Register the receiver using the application context.
-        IntentFilter filter = new IntentFilter(ACTION_CUSTOM_BROADCAST);
-        getApplicationContext().registerReceiver(mReceiver, filter);
-
-        methodChannel.setMethodCallHandler((call, result) -> {
-            if (call.method.equals("receiveKeyEvent")) {
-                String data = call.arguments();
-                // Log.d(TAG, "Data received from KeyEventReceiver: " + data);
-                        Log.i(TAG, "Data received: ddsdsdsd " + data);
-
-                result.success("Received key event: " + data);
-            
-            } else {
-                result.notImplemented();
-            }
-        });
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(refreshReceiver, new IntentFilter("com.example.sandbox.REFRESH_UI"));
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        getApplicationContext().unregisterReceiver(mReceiver);
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(refreshReceiver);
     }
 
-  
+    private BroadcastReceiver refreshReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            new MethodChannel(getFlutterEngine().getDartExecutor().getBinaryMessenger(), CHANNEL)
+                    .invokeMethod("refreshUI", null);
+        }
+    };
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 // import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return const MaterialApp(
       home: MyHomePage(),
     );
@@ -40,14 +48,10 @@ class MyHomePageState extends State<MyHomePage> {
           children: [
             TextFormField(
               autofocus: true,
-              
               key: const Key('Username'),
               controller: _usernameController,
               decoration: const InputDecoration(labelText: 'Username'),
-
-              
             ),
-            
             TextField(
               key: const Key('Password'),
               controller: _passwordController,
@@ -60,3 +64,89 @@ class MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+// import 'package:flutter/services.dart';
+
+// Future<void> main() async {
+//   await dotenv.load(fileName: ".env");
+
+//   runApp(
+//     const ProviderScope(
+//       child: MyApp(),
+//     ),
+//   );
+// }
+
+// class MyApp extends HookConsumerWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     return const MaterialApp(
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   MyHomePageState createState() => MyHomePageState();
+// }
+
+// class MyHomePageState extends State<MyHomePage> {
+//   final TextEditingController _usernameController = TextEditingController();
+//   final TextEditingController _passwordController = TextEditingController();
+
+//   static const platform = MethodChannel('com.example.sandbox_receiver/refresh');
+
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     platform.setMethodCallHandler((call) async {
+//       if (call.method == 'refreshUI') {
+//         setState(() {}); // Refresh the UI
+//       }
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     _usernameController.dispose();
+//     _passwordController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Login Screen'),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           children: [
+//             TextFormField(
+//               autofocus: true,
+//               key: const Key('Username'),
+//               controller: _usernameController,
+//               decoration: const InputDecoration(labelText: 'Username'),
+//             ),
+//             TextField(
+//               key: const Key('Password'),
+//               controller: _passwordController,
+//               decoration: const InputDecoration(labelText: 'Password'),
+//               obscureText: true,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
